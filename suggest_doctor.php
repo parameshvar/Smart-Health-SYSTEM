fdde97f42e08e21a6b61b4cbc0273b8fecb25a3f
parameshvar <104483668+parameshvar@users.noreply.github.com>
2022-04-27 10:58:52 +0530

    Add files via upload

---
+++ suggest_doctor.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <!--<link rel="stylesheet" href="suggest_doctor.css">-->
+<title>Disease Prediction</title>
+</head>
+
+<body class="container display-4">
+<div class="table-responsive" border="2" cellspacing="4" Cellpadding="10" align=center width="100%">
+  <table class="table" border="2" cellspacing="4" Cellpadding="10" align=center width="100%">
+    <!--Table header-->
+  <tr bgcolor="brown">
+    
+    <th><font color="white" align="center">Diseases and symptoms</th>
+  </tr>
+<!--table row 1-->
+  <tr bgcolor="lightblue">
+  <td><b><a href="doctors_list/medicine.php">A) GENERAL COMPLAINTS:</b></a> <br>Fever, Cough, Headache, Abdominal pain, Headache, Vomiting, Burning chest</td>
+  </tr>
+<!--table row 2-->
+  <tr>
+        <td><b><a href="doctors_list/cardiologist.php">B) CHEST & HEART COMPLAINTS: </b></a> <br>Chest pain & tightness, Breathlesness, Palpitation, Virtigo </td>
+  </tr>
+<!--table row 3-->
+  <tr bgcolor="lightblue">
+        <td><b><a href="doctors_list/gynecologist.php">C) PREGNANCY RELATED COMPLAINTS: </b></a> <br>P/V bleeding during pregnency,\nLess fatal movement, ecxcessive whitish discharge P/V, Severe lower abdominal pain puring menestrition, Lower abdominal pain</td>
+  </tr>
+<!--table row 4-->
+    <tr>
+        <td><b><a href="doctors_list/orthologist.php">D) BONE RELATED COMPLAINTS: </b></a> <br>Fractures, Dislocation, Joint pain, Swelling of joint, Bone Pain, Swelling leg, Neck-pain, Knee-pain</td>
+	</tr>
+<!--table row 5-->
+    <tr bgcolor="lightblue">
+        <td><b><a href="doctors_list/dentist.php">E) TOOTH COMPLAINTS: </b></a> <br>Bleeding gum, Gum-swelling, Toothache, carries teeth</td>
+	</tr>
+<!--table row 6-->
+    <tr>
+        <td><b><a href="opthamo.php">F) EYE COMPLAINTS: </b></a> <br>Eye pains and Alergies, Eye redness, Yellow-eye, Blurred Vision, Light Sensitivity, Dry Eyes, Excess Tearing, Cataract</td>
+	</tr>
+<!--table row 7-->
+    <tr bgcolor="lightblue">
+        <td><b><a href="dermo.php">G)SKIN COMPLAINTS: </b></a> <br>Itching, Skin-Lashion, Dry & peeling skin, Red-White or pus-filled bumps.</td>
+	</tr>
+<!--table row 8-->
+    <tr>
+        <td><b><a href="gastro.php">H)DIGESTIVE & GAS COMPLAINTS:</a> </b> <br>Constipation, Loose-motion, Alcholic Liver Diseases, Anal fissures</td>
+	</tr>
+<!--table row 9-->
+    <tr bgcolor="lightblue">
+        <td><b><a href="podia.php">I)LOWER LEG & ANKLE COMPLAINTS: </b></a> <br>Swelling leg, feet or lower leg problems, ankle twist, nail infections</td></tr>
+    </tr>
+<!--table footer-->
+  <tfoot>
+    <tr>
+      <th colspan= "4">*Select your complaints to consult specialists</th>
+    </tr>
+  </tfoot>
+  </table>
+</div>
+<div class="container ">
+<ul class="pager font-weight-bold text-monospace">
+  <li class="previous "><a href="Users/view_user_home_page.php">Previous Page</a></li>
+</ul></div>
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+</html>
